d6b14ed0b1633cb600bbdeea295952e938d28f70
HamjaMohtadee <48330979+whitehacker969@users.noreply.github.com>
2025-01-27 23:15:07 +0600

    package scope example added

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
-import "fmt"
 
-func main(){
-	fmt.Println("hello ")
-} 
+import (
+	"fmt"
+
+	"example.com/mathlib"
+)
+
+func main() {
+	fmt.Println("showing custom package")
+	mathlib.Add(1, 2)
+
+}
